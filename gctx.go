486273e889aaf3e54fff0cbe97b671e78d6f605d
Fred Baube <fbaube@welho.com>
2019-11-16 17:52:37 +0200

    New utility capabilities based on stdlib Context and Logger.

---
+++ gctx.go
@@ -0,0 +1,33 @@
+package miscutils
+
+import (
+	"context"
+
+	// "github.com/fbaube/glog"
+	"github.com/jmoiron/sqlx"
+)
+
+// GCtx carries background info thru a thread or goroutine.
+// Golang best practices for `Context` say that
+// 1) It should not be embedded anywhere, but rather should
+// be passed as the first argument to func calls, and
+// 2) It should contain only request-scoped data.
+// But we won't abide by those rules for Context because
+// 1) We only use it for cancellation, and
+// 2) We want zero-argument method calls, and
+// 3) We also store data that is NOT request-scoped.
+//
+type GCtx struct {
+	// Context, used mainly for cancellation
+	context.Context // = context.Context.TODO
+	// Database connection
+	*sqlx.DB
+	// Database transaction
+	*sqlx.Tx
+	// Own Log Prefix (should include a blank if needed)
+	OLP string
+	// Own Logger
+	Log *Logger
+	// Session Logger (shared by all input files)
+	SsnLog *Logger
+}

60e06d37f391173a7d181549695546c59a4b24b0
Fred Baube <fbaube@welho.com>
2022-09-17 19:24:45 +0300

    Improve docu

--- errer.go
+++ errer.go
@@ -1,14 +1,14 @@
 package miscutils
 
-// Error is used to embed an error in a struct,
+// Errer is used to embed an error in a struct,
 // when we want to execute (pointer) methods on
-// a struct pipeline-style, i.e. chainable, and
-// executed left-to-right.
+// a struct in the style of a data pipeline,
+// i.e. chainable, and executed left-to-right.
 //
 // We make the error public so that it is easily
-// set, so that we can wrap errors easily using
-// the "%w" printf format spec.
-//
+// set, and so that we can wrap errors easily
+// using the "%w" printf format spec.
+// .
 type Errer struct {
 	Err error
 }
@@ -16,12 +16,15 @@ type Errer struct {
 // HasError is a convenience function.
 // Since Err is publicly visible, HasError is
 // not really needed, but it seems appropriate
-// given that we also have func Error() .
+// given that we also have func Error()
+// .
 func (p Errer) HasError() bool {
 	return p.Err != nil
 }
 
-// Error is NPE-proof.
+// Error is an NPE-proof improvement
+// on the standard error.Error()
+// .
 func (p Errer) Error() string {
 	if p.Err == nil { // !p.HasError() {
 		return ""
@@ -29,9 +32,5 @@ func (p Errer) Error() string {
 	return p.Err.Error()
 }
 
-/*
-// SetError is unnecessary if Err is publicly visible.
-func (p *Errable) SetError(e error) {
-	p.Err = e
-}
-*/
+// func (p *Errable) SetError(e error)
+// is unnecessary if Err is publicly visible.
